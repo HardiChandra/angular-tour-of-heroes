65cc47c24bd7c20310b63326812722651b47b939
Hardi Chandra <hchandra@transcendinsights.com>
2017-11-09 15:30:17 -0800

    7. Routing completed

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,27 +1,23 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Hero } from '../hero';
-import { HeroService } from '../hero.service';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { Hero } from "../hero";
+import { HeroService } from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
+  styleUrls: [ './heroes.component.css' ],
   encapsulation: ViewEncapsulation.None
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService) {
+  }
 
   ngOnInit() {
     this.getHeroes();
