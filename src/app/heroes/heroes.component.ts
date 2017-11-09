b85a68b49ac5bbab7604e9beb95e6c76db99c1d5
Hardi Chandra <HC@Sainou.local>
2017-11-09 00:45:23 -0800

    Basic angular tutorial

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
